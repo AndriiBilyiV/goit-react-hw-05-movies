f408a21316a69591e2f81be38b47bd5466eecc77
AndriiBilyiV <andrey.dobraf@gmail.com>
2023-11-23 01:27:15 +0200

    clear imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from "react"
 import { Route, Routes } from "react-router-dom"
-import { FetchSearch, FetchTrends } from "./api"
 import { Header } from "./Header/Header"
 import { SearchBar } from "../pages/SearchBar/SearchBar"
-import { SearchList } from "./SearchList/SearchList"
 import { Trending } from "../pages/Trending/Trending"
 import { Movie } from "pages/Movie/Movie"
-import { Cast } from "./Cast/Cast"
 
 export const App = () => {
 
